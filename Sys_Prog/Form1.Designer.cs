b717ca59db18a87d3dbf920c0090e7e6d203a9cf
Viktor Malysh <viktor.motodom@gmail.com>
2022-02-23 18:13:36 +0200

    Add project files.

---
+++ Sys_Prog/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Sys_Prog
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonStart2 = new System.Windows.Forms.Button();
+            this.buttonStop2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(93, 67);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(550, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonStop.Location = new System.Drawing.Point(519, 130);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(101, 37);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonStart.Location = new System.Drawing.Point(108, 130);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(92, 37);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(519, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart2
+            // 
+            this.buttonStart2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonStart2.Location = new System.Drawing.Point(108, 200);
+            this.buttonStart2.Name = "buttonStart2";
+            this.buttonStart2.Size = new System.Drawing.Size(92, 32);
+            this.buttonStart2.TabIndex = 3;
+            this.buttonStart2.Text = "Start";
+            this.buttonStart2.UseVisualStyleBackColor = true;
+            this.buttonStart2.Click += new System.EventHandler(this.buttonStart2_Click);
+            // 
+            // buttonStop2
+            // 
+            this.buttonStop2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonStop2.Location = new System.Drawing.Point(519, 200);
+            this.buttonStop2.Name = "buttonStop2";
+            this.buttonStop2.Size = new System.Drawing.Size(101, 32);
+            this.buttonStop2.TabIndex = 4;
+            this.buttonStop2.Text = "Stop";
+            this.buttonStop2.UseVisualStyleBackColor = true;
+            this.buttonStop2.Click += new System.EventHandler(this.buttonStop2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStop2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonStart2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonStart2;
+        private System.Windows.Forms.Button buttonStop2;
+    }
+}
+
---
+++ Sys_Prog/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sys_Prog
+{
+    public partial class Form1 : Form
+    {
+        private bool stopPressed;
+        private Thread progressThread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            stopPressed = false;
+            if (progressThread == null)
+            {
+                progressThread = new Thread(ShowProgress); //создание обьекта - потока
+                progressThread.Start(); //начало выполнения потока
+            }
+
+        }
+
+        private void ShowProgress()
+        {
+
+            for (int i = 0; i <= 10; i++)
+            {
+                this.Invoke(
+                    new Action(
+                        ()=> {
+                            progressBar1.Value = i * 10; 
+                }));
+                Thread.Sleep(500);
+                if (stopPressed)
+                {
+                    progressThread = null;
+                    break;
+                } 
+            }
+            progressThread = null;
+        }
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            stopPressed = true;
+        }
+
+        private Thread progressThread2 = null;
+        private void ShowProgress2(object pauseTime)
+        {
+            int pTime = Convert.ToInt32(pauseTime);
+            for (int i = 0; i <= 10; i++)
+            {
+                this.Invoke(
+                    new Action(
+                        () =>
+                        {
+                            progressBar1.Value = i * 10;
+                        }));
+                Thread.Sleep(pTime);
+            }
+        }
+        private void buttonStart2_Click(object sender, EventArgs e)
+        {
+            progressThread2 = new Thread(ShowProgress2);
+            progressThread2.Start(300);
+        }
+
+        private void buttonStop2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
